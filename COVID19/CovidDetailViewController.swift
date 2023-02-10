e7c525cf0f2af2b1569a7d8abfd61af2bacb330c
KimKU1018 <86593582+KimKU1018@users.noreply.github.com>
2023-02-10 10:39:51 +0900

    ✨ 화면 UI 구성 완료

---
+++ COVID19/CovidDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CovidDetailViewController.swift
+//  COVID19
+//
+//  Created by KU Kim on 2023/02/10.
+//
+
+import UIKit
+
+class CovidDetailViewController: UITableViewController {
+
+    @IBOutlet weak var newCaseCell: UITableViewCell!
+    @IBOutlet weak var totalCaseCell: UITableViewCell!
+    @IBOutlet weak var recoveredCell: UITableViewCell!
+    @IBOutlet weak var percentageCell: UITableViewCell!
+    @IBOutlet weak var deathCell: UITableViewCell!
+    @IBOutlet weak var regionalOutbreakCell: UITableViewCell!
+    @IBOutlet weak var overseasInflowCell: UITableViewCell!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+
+}
